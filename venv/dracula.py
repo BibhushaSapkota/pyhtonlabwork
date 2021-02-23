568a2bebb701fe7c8b33a7c2f79277c66d5dafb5
Bibhushasapkota <Bibhushasapkota5@gmail.com>
2021-02-23 12:07:24 +0545

    Initial commit

---
+++ venv/dracula.py
@@ -0,0 +1,4 @@
+base=float(input("enter the base"))
+height=float(input("enter the height"))
+area=(base*height)/2
+print("the area of rectangle is ",area)
---
+++ venv/dragon.py
@@ -0,0 +1,6 @@
+# write a program to print sum of three numbers
+firstnumber=float(input("enter first number"))
+secondnumber=float(input("enter second number"))
+thirdnumber=float(input("enter third number"))
+sum=firstnumber+secondnumber+thirdnumber
+print("the sum of three number is" (sum))
