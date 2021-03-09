754e01359fcae5c1b35087c2a922f97413a1204f
Bibhushasapkota <Bibhushasapkota5@gmail.com>
2021-03-09 12:14:07 +0545

    initial commit

---
+++ programming/dududu.py
@@ -0,0 +1,5 @@
+# write a python program to find the sum of the firt n positie integers.
+# sum=(n*(n+1))/2
+n=int(input("enter the n integer number:"))
+sum_of_n_positive_integer=(n*(n+1))/2
+print("the sum of n integer number is" ,sum_of_n_positive_integer)
