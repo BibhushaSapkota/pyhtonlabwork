82435020febc805a66e55162ce11ddde9b392363
Bibhushasapkota <Bibhushasapkota5@gmail.com>
2021-03-16 11:23:11 +0545

    initial commit

--- programming/dududu.py
+++ programming/dududu.py
@@ -3,3 +3,8 @@
 n=int(input("enter the n integer number:"))
 sum_of_n_positive_integer=(n*(n+1))/2
 print("the sum of n integer number is" ,sum_of_n_positive_integer)
+
+
+list=[1,2,3,4,5,6,7,8,9,10]
+total=sum(list)
+print(f"the sum of given positive integer is {total}")
---
+++ programming/hshuhjsnldj.py
@@ -0,0 +1,7 @@
+#write a python program to convert seconds to day,hour,minutes and seconds
+seconds=int(input("enter the seconds"))
+days=(((seconds//60)//60)//24)
+hour=((seconds//60)//60)
+minutes=(seconds)//60
+
+print("its",days,"day",hour,"hour",minutes,"minutes")
